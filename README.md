9bc8301d93b2719773c72d76bf861c02bd60fb1c
Gagan Devagiri <gagandevagiri@gmail.com>
2018-11-29 13:27:25 +0000

    Added lsystem haskell file

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Lsystems-in-haskell
+﻿
+
+# L-systems in Turtle Graphics and Haskell
+
+### Getting started:
+
+The Swedish biologist Aristid Lindenmayer developed L-Systems to model the development of plants. An L-System consists of a start pattern and a set of rewrite rules which are recursively applied to the pattern to produce further increasingly complex patterns.
+
+You can read more about it here: https://en.wikipedia.org/wiki/L-system (definitely worth the read)
+
+
+## Prerequisites:
+
+To produce the results of my code you need to have Haskell and Turtle Graphics installed in your PC.
+- You can install Haskell from here: https://www.haskell.org/downloads
+Then, you can install turtle graphics by:
+	- Opening you terminal and updating the cabal database. You can do that by typing 
+	- ``cabal update``
+- Once it's updated, you can type:
+-  	``cabal install GLUT``
+- Download the code from Github, load it and you'll can type 
+- ``display ( {design of your choice} {int}).``
+
+## Author:
+ - Gagan Devagiri
+
+## License:
+- MIT License
+
+
+
---
+++ lsystem-haskell.hs
@@ -0,0 +1,116 @@
+-- Made by: Gagan Devagiri
+-- For: Introduction To Computation: Tutorial 6
+-- Date: 2nd November 2018
+
+
+module Tutorial6 where
+import LSystem
+import Test.QuickCheck
+import Data.Char
+
+
+--snowflake:
+    -- angle: 60
+    -- start: f--f--f-
+    -- rewrite: f → f+f--f+f
+
+snowflake :: Int -> Command
+snowflake x = f(x) :#: angle :#: angle :#: f(x) :#: angle :#: angle :#: f(x) :#: angle :#: angle
+  where
+    f 0 = GrabPen white :#: Go 10
+    f x = f(x-1) :#: angle' :#: f(x-1) :#: angle :#: angle :#: f(x-1) :#: angle' :#: f(x-1)
+    angle = Turn (-60)
+    angle' = Turn (60)
+
+-- -- Peano-Gosper:
+--        angle: 60
+--        start: f
+--        rewrite: f → f+g++g-f--ff-g+ g → -f+gg++g+f--f-g
+
+peanoGosper x = f x
+    where
+      f 0 = GrabPen purple :#: Go 10
+      f x = f (x-1) :#: n :#: g (x-1) :#: n
+          :#: n :#: g (x-1) :#: p :#: f (x-1)
+          :#: p :#: p :#: f (x-1) :#: f (x-1)
+          :#: p :#: g (x-1) :#: n
+
+      g 0 = GrabPen white :#: Go 10
+
+      g x = p :#: f (x-1)
+            :#: n :#: g (x-1)
+            :#: g (x-1) :#: n
+            :#: n :#: g (x-1) :#: n
+            :#: f (x-1) :#: p :#: p
+            :#: f (x-1) :#: p :#: g (x-1)
+
+      n = Turn 60
+      p = Turn(-60)
+--------------
+
+-- Cross:
+    -- angle: 90
+    -- start: f-f-f-f
+    -- rewrite: f → f-f+f+ff-f-f+f
+
+cross x = f x :#: n :#: f x :#: n :#: f x :#: n :#: f x
+    where
+      f 0 =  GrabPen white :#: Go 10
+
+      f x = f (x-1) :#: n :#: f (x-1) :#: p
+            :#: f (x-1) :#: p :#: f (x-1)
+            :#: f (x-1) :#: n :#: f (x-1)
+            :#: n :#: f (x-1) :#: p :#: f (x-1)
+
+      n = Turn 90
+      p = Turn(-90)
+
+--------
+
+-- Branch:
+    -- angle: 22.5
+    -- start: g
+    -- rewrite: g → f-[[g]+g]+f[+fg]-g f → ff
+
+branch x = g x
+   where
+     g 0 = GrabPen purple :#: Go 10
+     g x = f (x-1) :#: p :#: Branch (Branch (g (x-1))
+            :#: n :#: g (x-1)) :#: f (x-1)
+            :#: Branch (n :#: f (x-1) :#: g (x-1)) :#: p :#: g (x-1)
+
+     f 0 = GrabPen white :#: Go 10
+     f x = f (x-1) :#: f (x-1)
+
+     n = Turn 22.5
+     p = Turn(-22.5)
+
+---------
+
+-- 32-segment:
+    -- angle: 90
+    -- start: F+F+F+F
+    -- rewrite: F → -F+F-F-F+F+FF-F+F+FF+F-F-FF+ FF-FF+F+F-FF-F-F+FF-F-F+F+F-F+
+
+
+thirtytwo x = f x :#: n :#: f x :#: n :#: f x :#: n :#: f x
+    where
+      f 0 = GrabPen white :#: Go 10.0
+
+      f x =  p :#: f (x-1) :#: n
+            :#: f (x-1) :#: p :#: f (x-1)
+            :#: p :#: f (x-1) :#: n :#: f (x-1)
+            :#: n :#: f (x-1) :#: f (x-1) :#: p
+            :#: f (x-1) :#: n :#: f (x-1) :#: n
+            :#: f (x-1) :#: f (x-1) :#: n :#: f (x-1)
+            :#: p :#: f (x-1) :#: p :#: f (x-1) :#: f (x-1)
+            :#: n :#: f (x-1) :#: f (x-1) :#: p
+            :#: f (x-1) :#: f (x-1) :#: n :#: f (x-1) :#: n :#: f (x-1)
+            :#: p :#: f (x-1) :#: f (x-1) :#: p :#: f (x-1)
+            :#: p :#: f (x-1) :#: n :#: f (x-1)
+            :#: f (x-1) :#: p :#: f (x-1) :#: p :#: f (x-1)
+            :#: n :#: f (x-1) :#: n
+            :#: f (x-1) :#: p :#: f (x-1) :#: n
+
+      n = Turn 90
+      p = Turn (-90)
